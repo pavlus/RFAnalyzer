403c9b5bfcf45183522edc07ef0e020cae8d82c5
Pavel Remygailo <pavlusrem@gmail.com>
2016-10-28 20:03:11 +0300

    HiQSDR test server, requires IQ record. with packets 1442 bytes each.

--- tools/server.py
+++ tools/server.py
@@ -4,6 +4,7 @@ import time
 import socket
 from errno import EWOULDBLOCK
 import random
+
 packet_loose_prob=0.0 # probability of loosing packet from 0 to 1
 
 refClk = 122880000
@@ -80,7 +81,6 @@ print 'RX interface on ' + str(host) + ':' + str(rxPort)
 
 send = False
 file = open(dump_name, "rb")
-
 burst_cnt = 256;
 burst_iter = 0;
 while True:
