e257b460b5fe863c55bf2b079a1bbe673a6b7cb2
Pavel Remygailo <pavel.remygailo+github@gmail.com>
2017-02-16 14:06:47 +0200

    New tests and benchmarks

--- app/src/androidTest/java/com/mantz_it/rfanalyzer/FilterBenchmark.java
+++ app/src/androidTest/java/com/mantz_it/rfanalyzer/FilterBenchmark.java
@@ -25,7 +25,6 @@ public class FilterBenchmark extends ApplicationTestCase<Application> {
 		super(Application.class);
 	}
 
-
 	public void _testSizes() {
 		final int[] samplesCnt = {1<<4, 1<<5, 1<<6, 1<<7, 1<<8, 1<<9, 1 << 10, 1 << 11, 1 << 12, 1 << 13, 1 << 14, 1 << 15, 1 << 16, 1<<17};
 		int sampleRate = 1000000;
