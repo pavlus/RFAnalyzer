34e66573fa488f3ccc1c20e687f2a6e919543d01
Pavel Remygailo <pavel.remygailo+github@gmail.com>
2017-03-29 20:13:39 +0300

    Assertion treshold, readable names

--- app/src/test/java/com/mantz_it/rfanalyzer/dsp/spi/FIR_CRR2Test.java
+++ app/src/test/java/com/mantz_it/rfanalyzer/dsp/spi/FIR_CRR2Test.java
@@ -1,42 +1,43 @@
 package com.mantz_it.rfanalyzer.dsp.spi;
 
 import com.mantz_it.rfanalyzer.dsp.FIRDesigner;
-import com.mantz_it.rfanalyzer.dsp.FilterBuilder;
 import com.mantz_it.rfanalyzer.dsp.Util;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-
 /**
  * Created by Pavel on 18.12.2016.
  */
 public class FIR_CRR2Test {
+
+	static final float LINEAR_FILTER_DELTA = 1E-6f;
+
 	@Test
 	public void apply() throws Exception {
 		final int samplesCnt = 1024;
 		final int decimation = 2;
 		float[] taps = FIRDesigner.low_pass(1, samplesCnt, samplesCnt / (4 * decimation), samplesCnt / (4 * decimation), 60, Window.Type.WIN_BLACKMAN, 0);
-		FIR_CRR f1 = new FIR_CRR(taps, 2);
-		System.out.printf("%%Designed filter %s linear phase response.\n", FilterBuilder.isLinearPhase(f1.getTaps(),1e-6f)?"has":"has not");
-		FIR_CRR_LinearPhase f2 = new FIR_CRR_LinearPhase(f1);
-		System.out.printf("TapsPrototype = %s;\n", Arrays.toString(f1.getTaps()));
-		System.out.printf("TapsCountPrototype = %d;\n", f1.tapsCount);
-		System.out.printf("TapsLinearPhase = %s;\n", Arrays.toString(f2.getTaps()));
-		System.out.printf("TapsCountLinearPhase = %d;\n", f2.tapsCount);
+		FIR_CRR golden = new FIR_CRR(taps, 2);
+		//System.out.printf("%%Designed filter %s linear phase response.\n", FilterBuilder.isLinearPhase(golden.getTaps(),1e-6f)?"has":"has not");
+		FIR_CRR_LinearPhase tested = new FIR_CRR_LinearPhase(golden);
+		//System.out.printf("TapsPrototype = %s;\n", Arrays.toString(golden.getTaps()));
+		//System.out.printf("TapsCountPrototype = %d;\n", golden.tapsCount);
+		//System.out.printf("TapsLinearPhase = %s;\n", Arrays.toString(tested.getTaps()));
+		//System.out.printf("TapsCountLinearPhase = %d;\n", tested.tapsCount);
 		float[] interleavedSamples = new float[samplesCnt << 1];
 		Util.noise(interleavedSamples);
 		Packet in = new Packet(interleavedSamples);
-		float[] y1 = Util.applyFilter(f1, in, samplesCnt, decimation);
-		float[] y2 = Util.applyFilter(f2, in, samplesCnt, decimation);
-		Assert.assertArrayEquals(y1, y2, 1e-6f);
+		float[] out_golden = Util.applyFilter(golden, in, samplesCnt, decimation);
+		float[] out_tested = Util.applyFilter(tested, in, samplesCnt, decimation);
+		Assert.assertArrayEquals(out_golden, out_tested, LINEAR_FILTER_DELTA);
 		double err = 0;
-		for (int i = 0; i < y1.length; ++i) {
-			final float diff = y2[i] - y1[i];
+		for (int i = 0; i < out_golden.length; ++i) {
+			final float diff = out_tested[i] - out_golden[i];
 			err += diff * diff;
 		}
-		System.out.printf("TotalError = %e;", err);
+		Assert.assertEquals(err, 0, LINEAR_FILTER_DELTA);
+		//System.out.printf("TotalError = %e;", err);
 	}
 
 
